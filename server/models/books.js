160f2d99d406db9d0a44ba2cb65d18a97db97594
Luke Nguyen <lukenagooyen@gmail.com>
2020-10-28 14:52:54 -0400

    first commit

---
+++ server/models/books.js
@@ -0,0 +1,16 @@
+/* COMP229-W2020-MidTerm_Luke Nguyen_300744804_comp229midterm  */
+let mongoose = require('mongoose');
+
+// create a model class
+let Book = mongoose.Schema({
+    Title: String,
+    Description: String,
+    Price: Number,
+    Author: String,
+    Genre: String
+},
+{
+  collection: "books"
+});
+
+module.exports = mongoose.model('Book', Book);
---
+++ server/routes/index.js
@@ -0,0 +1,18 @@
+/* COMP229-W2020-MidTerm_Luke Nguyen_300744804_comp229midterm  */
+// modules required for routing
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+// define the game model
+let book = require('../models/books');
+
+/* GET home page. wildcard */
+router.get('/', (req, res, next) => {
+  res.render('content/index', {
+    title: 'Home',
+    books: ''
+   });
+});
+
+module.exports = router;
